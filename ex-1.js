56860a043caa89eb0360b6b0eba88cfaf267ba75
CharlieHii <charlie_chaper@hotmail.com>
2024-02-02 20:42:22 +0700

    feat: first attempt

--- ex-1.js
+++ ex-1.js
@@ -1,1 +1,27 @@
-function pivotIndex(nums) {}
+function pivotIndex(nums) {
+  const sum = nums.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
+  let index = 0;
+  let leftAccumulator = 0;
+  let rightAccumulator = sum;
+
+  while (index < nums.length) {
+    rightAccumulator -= nums[index];
+    if (leftAccumulator === rightAccumulator) {
+      return index;
+    }
+    leftAccumulator += nums[index];
+    index++;
+  }
+  return -1;
+}
+
+let nums1 = [1, 7, 3, 6, 5, 6];
+console.log(pivotIndex(nums1));
+
+let nums2 = [1, 2, 3];
+console.log(pivotIndex(nums2));
+
+let nums3 = [2, 1, -1];
+console.log(pivotIndex(nums3));
--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,9 @@
-function defangIPaddr(nums) {}
+function defangIPaddr(nums) {
+  return nums.split(".").join("[.]");
+}
+
+let address1 = "1.1.1.1";
+console.log(defangIPaddr(address1));
+
+let address2 = "255.100.50.0";
+console.log(defangIPaddr(address2));
